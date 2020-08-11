38fb4f1d989128c36af47b8768919d453bc128b8
stardiviner <numbchild@gmail.com>
2020-08-11 09:21:38 +0800

    add source code template

---
+++ org-extra-link-types.el
@@ -0,0 +1,39 @@
+;;; org-extra-link-types.el --- Add extra link types to Org Mode -*- lexical-binding: t; -*-
+
+;;; Time-stamp: <2020-08-11 09:09:01 stardiviner>
+
+;; Authors: stardiviner <numbchild@gmail.com>
+;; Package-Requires: ((emacs "25.1"))
+;; Package-Version: 0.1
+;; Keywords: org
+;; homepage: https://github.com/stardiviner/org-extra-link-types
+
+;; org-extra-link-types is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; org-extra-link-types is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+;;
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+
+
+
+
+
+
+(provide 'org-extra-link-types)
+
+;;; org-extra-link-types.el ends here
